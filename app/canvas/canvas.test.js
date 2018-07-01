446d06d55dca792dcdba4c9b9501fcf0379ace3f
vagfsantos <santos.vagnerferreira@gmail.com>
2018-07-01 16:09:54 -0300

    Canvas module - to do list of unit tests

---
+++ app/canvas/canvas.test.js
@@ -0,0 +1,22 @@
+import test from 'ava';
+import Canvas from '../../_test/canvas.js';
+
+test.todo('creates a default canvas element');
+
+test.todo('creates a custom canvas element');
+
+test.todo('creates just one canvas element');
+
+test.todo('returns canvas element reference');
+
+test.todo('returns canvas 2D context reference');
+
+test.todo('changes the configuration after creation');
+
+test.todo('validates the entries of custom configuration');
+
+test.todo('appends the canvas to another element');
+
+test.todo('preserves the integrity of canvas element');
+
+test.todo('preserves the integrity of 2D context');
